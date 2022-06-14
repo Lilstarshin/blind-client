4f66c4bd58af9e963ba5dbe34fce8c2de5ea19bd
lilstar <lilstar.shin@gmail.com>
2022-06-14 10:17:55 +0900

    modify backend url

--- plugins/api.js
+++ plugins/api.js
@@ -11,7 +11,7 @@ export default function ({ $axios }, inject) {
     })
 
     // Set baseURL to something different
-    api.setBaseURL('http://www.lilstarshin.com/')
+    api.setBaseURL('https://www.lilstarshin.com/')
 
     // Inject to context as $api
     inject('api', api)
